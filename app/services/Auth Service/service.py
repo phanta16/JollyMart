66e94fb9b5fef61dc7152d92fccfe3b113d19a7a
PhaNTa <alex2009oks@gmail.com>
2025-04-23 21:02:01 +1000

    Third commit!

--- app/services/Auth Service/service.py
+++ app/services/Auth Service/service.py
@@ -1,7 +1,8 @@
 import hashlib
 import secrets
 
-from flask import jsonify, request, Flask
+from flask import jsonify, request, Flask, make_response
+import requests
 
 import db_session
 from model import AuthInfo
@@ -22,9 +23,9 @@ def register():
                         hashed_password=hashlib.sha512(hashed_password.encode('utf-8')).hexdigest())
         session.add(user)
         session.commit()
-        return jsonify({'status': 'True', 'session_id': session_id, 'uid': user.uid})
-    except Exception as e:
-        return jsonify({'status': 'Something went wrong!', 'message': str(e)})
+        return make_response(jsonify({'status': 'True', 'session_id': session_id, 'uid': user.uid}), 200)
+    except requests.exceptions.RequestException as e:
+        return make_response(jsonify({'status': 'Something went wrong!', 'message': str(e)}), 404)
 
 
 @app.route('/auth/login/', methods=['POST'])
@@ -39,14 +40,14 @@ def login():
         user = session.get(AuthInfo, uid)
 
         if user is None:
-            return jsonify({'status': 'Такого пользователя не существует!'})
+            return make_response(jsonify({'status': 'Такого пользователя не существует!'}), 404)
         else:
             if user.hashed_password == hashlib.sha512(hashed_password.encode('utf-8')).hexdigest():
-                return jsonify({'status': 'True', 'session_id': user.session_id})
+                return make_response(jsonify({'status': 'True', 'session_id': user.session_id}), 200)
             else:
-                return jsonify({'status': 'False'})
-    except Exception as e:
-        return jsonify({'status': 'Something went wrong!', 'message': str(e)})
+                return make_response(jsonify({'status': 'False'}), 401)
+    except requests.exceptions.RequestException as e:
+        return make_response(jsonify({'status': 'Something went wrong!', 'message': str(e)}), 404)
 
 
 @app.route('/auth/change_password/', methods=['PATCH'])
@@ -64,11 +65,11 @@ def change_password():
         user.hashed_password = new_hashed_password
         session.commit()
 
-        return jsonify({'status': 'True'})
+        return make_response(jsonify({'status': 'True'}), 200)
 
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
 
-        return jsonify({'status': 'False'})
+        return make_response(jsonify({'status': 'False', 'message': str(e)}), 401)
 
 @app.route('/auth/delete_user/', methods=['DELETE'])
 def delete_user():
@@ -80,66 +81,72 @@ def delete_user():
         session.delete(user)
         session.commit()
 
-        return jsonify({'status': 'True'})
+        return make_response(jsonify({'status': 'True'}), 200)
 
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
 
-        return jsonify({'status': 'False'})
+        return make_response(jsonify({'status': 'False', 'message': str(e)}), 401)
 
 
-# ROUTES = {
-#     "/chat/": "http://chat-service:5001",
-#     "/profile/": "http://profile-service:5002"
-# }
-#
-# @app.route("/", defaults={"path": ""}, methods=["GET", "POST", "PUT", "DELETE"])
-# @app.route("/<path:path>", methods=["GET", "POST", "PUT", "DELETE"])
-# def gateway(path):
-#     # 1. Получить session_id из куки
-#     session_id = request.cookies.get("session_id")
-#     if not session_id:
-#         return jsonify({"error": "Missing session"}), 401
-#
-#     # 2. Найти uid в базе
-#     db = Session()
-#     session_row = db.query(SessionModel).filter_by(session_id=session_id).first()
-#     db.close()
-#     if not session_row:
-#         return jsonify({"error": "Invalid session"}), 403
-#
-#     uid = session_row.uid
-#
-#     # 3. Найти маршрут
-#     for prefix, target_url in ROUTES.items():
-#         if request.path.startswith(prefix):
-#             service_url = target_url + request.path[len(prefix)-1:]
-#             break
-#     else:
-#         return jsonify({"error": "Unknown path"}), 404
-#
-#     # 4. Подготовить заголовки
-#     headers = dict(request.headers)
-#     headers["X-User-Id"] = uid
-#     headers.pop("Host", None)
-#
-#     # 5. Проксировать
-#     try:
-#         response = requests.request(
-#             method=request.method,
-#             url=service_url,
-#             headers=headers,
-#             params=request.args,
-#             json=request.get_json(silent=True)
-#         )
-#     except requests.exceptions.RequestException as e:
-#         return jsonify({"error": "Service unavailable", "detail": str(e)}), 502
-#
-#     flask_response = make_response(response.content, response.status_code)
-#     for name, value in response.headers.items():
-#         flask_response.headers[name] = value
-#
-#     return flask_response
-#
+
+
+ROUTES = {
+
+    "/chat/": "http://chat-service:5000",
+    "/user/": "http://user-service:5000",
+    "/comment/": "http://comment-service:5000",
+    "/favourite/": "http://favourite-service:5000",
+    "/media/": "http://media-service:5000",
+    "/notifications/": "http://notifications-service:5000",
+    "/recommendations/": "http://recommendations-service:5000",
+    "/search/": "http://search-service:5000",
+    "/posts/": "http://posts-service:5000",
+    "/auth/": "http://auth-service:5000",
+}
+
+@app.route("/", defaults={"path": ""}, methods=["GET", "POST", "PUT", "DELETE"])
+@app.route("/<path:path>", methods=["GET", "POST", "PUT", "DELETE"])
+def gateway(path):
+
+    session_id = request.cookies.get("session_id")
+
+    if not session_id:
+        return jsonify({"error": "Forbidden!"}), 401
+
+    session = db_session.create_session()
+    user = session.query(AuthInfo).filter_by(session_id=session_id).first()
+    if not user:
+        return jsonify({"error": "Invalid session"}), 403
+
+    uid = user.uid
+
+    for prefix, target_url in ROUTES.items():
+        if request.path.startswith(prefix):
+            service_url = target_url + request.path[len(prefix)-1:]
+            break
+    else:
+        return jsonify({"error": "Unknown service!"}), 404
+
+    headers = dict(request.headers)
+    headers["X-User-Id"] = uid
+
+    try:
+        response = requests.request(
+            method=request.method,
+            url=service_url,
+            headers=headers,
+            params=request.args,
+            json=request.get_json(silent=True)
+        )
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": "Service unavailable!", "detail": str(e)}), 502
+
+    f_response = make_response(response.content, response.status_code)
+    for name, value in response.headers.items():
+        flask_response.headers[name] = value
+
+    return f_response
+
 
 app.config['SECRET_KEY'] = 'THE_MOST_SECRET_KEY_YOU_HAVE_EVER_SEEN'
 
