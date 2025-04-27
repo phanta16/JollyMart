8cca469a6d9d75e10445980b0519481c75ffa193
PhaNTa <alex2009oks@gmail.com>
2025-04-27 11:57:51 +1000

    Sixth commit!

---
+++ app/services/comment-service/model.py
@@ -0,0 +1,15 @@
+import datetime
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy import orm
+import sqlalchemy_serializer
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+class CommentaryInfo(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'auth_info'
+    comment_id = sqlalchemy.Column(sqlalchemy.String, nullable=False, primary_key=True, autoincrement=True)
+    post_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    comment_author_id = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    context = sqlalchemy.Column(sqlalchemy.String, nullable=False)
---
+++ app/services/comment-service/service.py
@@ -0,0 +1,89 @@
+from flask import request, Flask, make_response, jsonify
+
+import db_session
+from model import CommentaryInfo
+
+app = Flask(__name__)
+
+
+@app.route('/comment/all-comments', methods=['POST'])
+def all_comments():
+    request_data = request.get_json()
+
+    session = db_session.create_session()
+    post_id = request_data['post_id']
+
+    try:
+        comments = session.query(CommentaryInfo).filter_by(post_id=post_id).all()
+        return make_response(jsonify({
+                                         "comment_id": c.comment_id,
+                                         "post_id": c.post_id,
+                                         "comment_author_id": c.comment_author_id,
+                                         "context": c.context,
+                                     } for c in comments))
+    except Exception as e:
+        return make_response(jsonify({"error": str(e)}))
+
+
+@app.route('/comment/new-comment', methods=['POST'])
+def new_comment():
+    request_data = request.get_json()
+
+    session = db_session.create_session()
+    post_id = request_data['post_id']
+    comment_author_id = request_data['comment_author_id']
+    context = request_data['context']
+
+    try:
+        comment = CommentaryInfo(comment_author_id=comment_author_id, post_id=post_id, context=context)
+        session.add(comment)
+        session.commit()
+        return make_response(jsonify({"success": "True"}))
+
+    except Exception as e:
+        return make_response(jsonify({"error": str(e)}))
+
+
+@app.route('/comment/del-comment', methods=['DELETE'])
+def delete_comment():
+    request_data = request.get_json()
+
+    session = db_session.create_session()
+    post_id = request_data['post_id']
+    comment_author_id = request_data['comment_author_id']
+    context = request_data['context']
+
+    try:
+        comment = session.query(CommentaryInfo).filter_by(comment_author_id=comment_author_id, post_id=post_id,
+                                                          context=context).first()
+        session.delete(comment)
+        session.commit()
+        return make_response(jsonify({"success": "True"}))
+
+    except Exception as e:
+        return make_response(jsonify({"error": str(e)}))
+
+
+@app.route('/comment/change-comment', methods=['PATCH'])
+def change_comment():
+    request_data = request.get_json()
+
+    session = db_session.create_session()
+    post_id = request_data['post_id']
+    comment_author_id = request_data['comment_author_id']
+    context = request_data['context']
+    new_context = request_data['new_context']
+
+    try:
+        comment = session.query(CommentaryInfo).filter_by(comment_author_id=comment_author_id, post_id=post_id,
+                                                          context=context).first()
+        comment.context = new_context
+        session.commit()
+        return make_response(jsonify({"success": "True"}))
+
+    except Exception as e:
+        return make_response(jsonify({"error": str(e)}))
+
+
+db_session.global_init('db/JollyCommentDB.db')
+app.run(port=5002, host='0.0.0.0')
