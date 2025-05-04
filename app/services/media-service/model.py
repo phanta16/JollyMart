a50ede19caf0c449f3aa5f537df1c11b5ff1094d
PhaNTa <alex2009oks@gmail.com>
2025-05-04 21:57:13 +1000

    Thirteenth commit!

---
+++ app/services/media-service/model.py
@@ -0,0 +1,15 @@
+import datetime
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy import orm
+import sqlalchemy_serializer
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+class MediaInfo(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'media_info'
+    image_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    path = sqlalchemy.Column(sqlalchemy.String)
+    post_id = sqlalchemy.Column(sqlalchemy.Integer)
+    date = sqlalchemy.Column(sqlalchemy.DateTime)
