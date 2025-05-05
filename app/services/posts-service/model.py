f7027a6a03dfa9939a73320efc97645159d36823
PhaNTa <alex2009oks@gmail.com>
2025-05-05 13:58:20 +1000

    Fifteenth commit!

---
+++ app/services/posts-service/model.py
@@ -0,0 +1,16 @@
+import datetime
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy import orm
+import sqlalchemy_serializer
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+class PostInfo(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'posts_info'
+    post_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False, autoincrement=True, primary_key=True)
+    media = sqlalchemy.Column(sqlalchemy.BLOB, nullable=False)
+    date_created = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False, default=datetime.datetime.now)
+    author_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    text = sqlalchemy.Column(sqlalchemy.String, nullable=False)
